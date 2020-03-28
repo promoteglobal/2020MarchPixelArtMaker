0a6a7ada57d5b4c1b067f161713002afbee57f6f
Katherine Langley <promoteglobal@gmail.com>
2020-03-28 14:40:54 +0800

    code cleaned up and added comments

--- designs.js
+++ designs.js
@@ -1,38 +1,20 @@
-// Select color input
-// let colorElement = document.querySelector("#colorPicker");
-// let color;
-
-// colorElement.addEventListener('click', function (event) {
-//   debugger
-//   event.preventDefault();
-//   console.log("youclicked");
-//   color = colorElement.value;
-// });
-
-// function changeColor() {
-//   document.getElementById("pixelCanvas").style.backgroundColor = document.getElementById("colorPicker").value;
-//   console.log("You Changed the Color");
-// }
-
 const submitEl = document.querySelector("#submitbutton");
 const tableEl = document.querySelector("#pixelCanvas");
 
+//gathering color info and applying to grid
 tableEl.addEventListener('click', function (event) {
-console.log("clicked");
 event.target.style.backgroundColor = document.getElementById("colorPicker").value;
 });
 
 
-
 let height;
 let width;
-
+//gathering height and width numbers and creating a grid
 submitEl.addEventListener('click', function (event) {
   tableEl.innerHTML = '';
   const heightEl = document.querySelector("#inputHeight");
   const widthEl = document.querySelector("#inputWidth");
   event.preventDefault();
-  console.log("you clicked the Submit button");
   height = heightEl.value;
   width = widthEl.value;
 
@@ -40,28 +22,8 @@ submitEl.addEventListener('click', function (event) {
     const tr = document.createElement('tr');
     tableEl.appendChild(tr);
     for (var x = 0; x < width; x++) {
-      console.log(y, x);
       const td = document.createElement('td');
       tr.appendChild(td);
     }
   }
 });
-// Select size input
-
-
-// When size is submitted by the user, call makeGrid()
-
-
-// <!--
-// <table id="demo" onclick="changeColor()>
-// <tr>
-//   <td></td>
-//   <td></td>
-// </tr>
-// <tr>
-//   <td></td>
-// </tr>
-// </table>
-// -->
-
-{/* <h2 id="testElement" onclick="changeColor()">test</h2> */}
